fe40163a02031ea8ee6148931ae7af38411d8bdd
amirafzali <amir@afzali.ca>
2020-04-14 01:52:35 -0400

    Do some refactoring

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ addEventListener('fetch', event => {
 })
 
 /**
- * Helper function to return array of domains from API request
+ * Helper function to return array of variant domains from API request
  * @param {Request} request 
  */
-async function getDomains(request) {
-
+async function getVariants(request) {
   // Perform a GET request on the API and parse the returned JSON object.
-  const request = await fetch("https://cfw-takehome.developers.workers.dev/api/variants");
-  const requestJSON = await request.json();
+  const res = await fetch("https://cfw-takehome.developers.workers.dev/api/variants");
+  const resJSON = await res.json();
+  const { variants } = resJSON;
 
-  // Extract the URL values and pick a random one
-  return Object.values(requestJSON);
+  // Extract and return the variant URLs
+  return Object.values(variants);
+}
+
+async function prepareResponse(domain, variantNumber) {
 
 }
 
@@ -23,11 +26,30 @@ async function getDomains(request) {
  */
 async function handleRequest(request) {
 
-  const options = await getDomains(request);
+  const returnInfo = { headers: { 'content-type': 'text/html;charset=UTF-8' } };
 
-  const selectedDomain = Math.random < 0.5 ? options[0]:options[1];
+  // Try to grab a cookie on the request header
+  const cookie = request.headers.get('cookie');
+  const ID = "saved-response";
 
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  // Retrieve the domain options and pick a random one (should be random enough for our purposes)
+  const variants = await getVariants(request);
+  const choice = Math.random() < 0.5 ? 0:1;
+  const randomDomain = variants[choice];
+  const cookieValue = choice === 1 ? 'first':'second';
+
+  // Check if the cookie exists
+  if(cookie) {
+    if(cookie.includes(`${ID}=first`)) {
+      return new Response(formFirst());
+    } else if(cookie.includes(`${ID}=second`)) {
+      return new Response(f);
+    }
+  }
+
+  // Remember the decision for next time.
+  //response.headers.append('Set-Cookie', `${ID}=${cookieValue}; path=/`);
+  const res = await fetch(randomDomain);  
+  console.log(randomDomain);
+  return new Response(await res.text(), returnInfo);
 }
